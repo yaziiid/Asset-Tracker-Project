051969baf9b0ab093714019d638dab3b3cf62212
Yazid <90795659+yaziiid@users.noreply.github.com>
2022-12-01 12:53:41 +0100

    reduce header footer size

--- dash_app/__init__.py
+++ dash_app/__init__.py
@@ -27,26 +27,26 @@ navbar = dbc.Navbar(
             html.A([
                 dbc.Row([
                     dbc.Col([
-                        html.Img(src='assets/tbcn-logo2.png', width=200, height=100,className='navbar-brand rounded float-start'),
+                        html.Img(src='assets/tbcn-logo2.png', width=160, height=90,className='mt-3 navbar-brand rounded float-start'),
                         html.Small('To Be Connected Nigeria', className='light')
-                    ], class_name='col-3 align-center'),
+                    ], class_name='col-3 align-center py-0'),
                 ], align='center', className='g-0'),
             ], href='/'),
         ], align='start', class_name='col-3'),
         dbc.Col([
-            html.H2('Save-80 Geo-locator'), #style={'color':'#B8E1E9'}
+            html.H2('Save-80 Geo-locator',), #style={'color':'#B8E1E9'}
             dbc.NavbarToggler(id='nav-toggler', n_clicks=0),
             dbc.Collapse(nav, id='navbar-collapse', is_open=False, navbar=True),
-        ],class_name='col-6 text-center header-text align-center'),
+        ],class_name='col-6 text-center header-text align-center gy-0'),
         dbc.Col([
              dcc.Dropdown(
                 id = 'home_search', placeholder= 'search name or product id ', className='dropdown nav-search me-auto', optionHeight=50,
             )
         ], class_name='ms-auto mt-3'),
         dbc.Col(
-            html.Img(src='assets/atmosfair.png', width=200, height=100, className='navbar-brand rounded atmos_logo float-end') #className='navbar-brand rounded float-end atmos_logo
+            html.Img(src='assets/atmosfair2.png', width=200, height=90, className='mt-4 navbar-brand rounded atmos_logo float-end') #className='navbar-brand rounded float-end atmos_logo
         )
-    ], fluid=True, class_name='d-flex justify-content-center')
+    ], fluid=True, class_name='d-flex justify-content-center py-0')
 ,id='navbar', class_name='navbar')
 
 
@@ -55,7 +55,7 @@ FOOTER_STYLE = {
     "bottom": 0,
     "left": 0,
     "right": 0,
-    'height':'80px',
+    'height':'50px',
     # 'background': '#bdc3c7',
     # 'background': '-webkit-linear-gradient(to top, #2c3e50, #bdc3c7)',
     # 'background': 'linear-gradient(to top, #2c3e50, #bdc3c7)',
@@ -80,13 +80,13 @@ main_layout = dbc.Container([
     dbc.Row([
         dbc.Col([
             # html.Small('13b, Mambila Street, Aso Drive, Abuja.', className='m-info d-inline'), #, style={'color':'#B8E1E9'}
-            html.A('www.tbcn.com.ng', href='http://tbcn.com.ng', className = 'm-info me-auto',style={'color':'#00738A'}) #style={'color':'#B8E1E9'}
-        ], class_name='info_footer', style={'padding-left':'30px'}),
+            html.A('www.tbcn.com.ng', href='http://tbcn.com.ng', className = 'm-info me-auto  mt-4',style={'color':'#00738A'}) #style={'color':'#B8E1E9'}
+        ], class_name='info_footer ', style={'padding-left':'30px'}),
         dbc.Col([
             html.H3('powered by Metaverse®', className='footer_text mt-4', style={'color':'#1E1D1E'})
         ], class_name='text-center footer mt-0'),
         dbc.Col([
-            html.Small('TBCN® ©2022', className='m-info ms-auto'), #, style={'color':'#B8E1E9'}
+            html.Small('TBCN® ©2022', className='m-info ms-auto  mt-4'), #, style={'color':'#B8E1E9'}
             # html.Small('2022©', className='ms-auto') #, style={'color':'#B8E1E9'}
         ], class_name='text-center info_footer', style={'padding-right':'30px'})
     ], class_name='d-flex justify-content-center bg-light',  style=FOOTER_STYLE)    
